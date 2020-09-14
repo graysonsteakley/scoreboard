89006f1534d137898dbbb4e44da60fcc974562da
grayson <grayson.steakley@gmail.com>
2020-09-14 12:49:42 -0500

    fixed key duplication error through sorting

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -6,11 +6,10 @@ import ScoreItems from './ScoreItems';
 const ScoreBoardContainer = styled.div`
 
     width: 100%;
-    height: auto;
+    height: 60vh;
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
 
 
 `;
@@ -31,27 +30,49 @@ export default class ScoreBoard extends Component {
 
 
 
+
     addItem(e){
         e.preventDefault();
+
         if(this._nameElement.value !== "" && this._scoreElement.value !== ""  && !isNaN(this._scoreElement.value)){
+
             var newItem = {
                 name: this._nameElement.value,
                 score: this._scoreElement.value,
                 key: Date.now()
             };
             
+            //ADDS THE NEW SCORE OBJECT AND SORTS THE NEW STATE
             this.setState((prevState)=>{
-                return{
-                    items: prevState.items.concat(newItem).sort(
-                        (a, b) => {
-                            return b.score - a.score;
-                        }
-                    )
-                };
+                if(this.state.items.length < 9){                
+                    return{
+                        items: prevState.items.concat(newItem).sort(
+                            (a, b) => {
+                                return b.score - a.score;
+                            }
+                        )
+                    };
+                }
+                else{
+                    
+                    //REMOVES THE CURRENT LOWEST VALUE AND REPLACES WITH NEW ONE AFTER LIST GETS TO LARGE
+                    const b = prevState.items;
+                    b.pop();
+
+                    return{
+                           items: b.concat(newItem).sort(
+                            (a, b) => {
+                                return b.score - a.score;
+                            }
+                        )
+                    }
+
+                }
+
             });
         }
-        console.log(this.state.items);
 
+        //RESETS INPUT VALUES
         this._nameElement.value = "";
         this._scoreElement.value = "";
 
@@ -60,7 +81,7 @@ export default class ScoreBoard extends Component {
     render() {
         return (
             <ScoreBoardContainer>
-                <form onSubmit={this.addItem}>
+                <form onSubmit={this.addItem} className="ml-5">
                     <input 
                     ref={(a) => this._nameElement = a}
                     placeholder="Enter Name:"/>
