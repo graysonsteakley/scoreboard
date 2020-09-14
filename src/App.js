b92d7478db13983446a39501e1bd8ee578f0298c
grayson <grayson.steakley@gmail.com>
2020-09-14 09:19:22 -0500

    added background and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components'
 import 'bootstrap/dist/css/bootstrap.css';
+import {ScoreBoard} from './components/ScoreBoard'
 
 const Header = styled.h1`
 
-  font-size: 4rem;
+  font-size: 20rem;
   color: rgb(210, 31, 84)!important;
   font-weight: bolder;
-  letter-spacing: 1.2;
+  letter-spacing: 2.5;
+  text-align: center;
+
 
 `;
 
 
 
 function App() {
+
+  const [HoverState, SetHoverState] = useState(false);
+
+  const handleMouseOver = (e, ind)=>{
+    SetHoverState(!HoverState);
+  }
+  
+      
+  const addText = (inputString)=>{
+          
+
+    const ScoreString = inputString.split(' ');
+    const Scores = ScoreString.map((letter, ind) => {
+        return (
+            
+            <span onMouseOver={(e, ind)=>handleMouseOver(e, ind) }
+            onMouseLeave={(e, ind)=>handleMouseOver(e, ind) }
+            className = {`${HoverState ? `main-text hover${ind}`: `light-text text-up-anim2 hover${ind}`}`} style={{display: 'inline', cursor: 'pointer'}} key={ind} >
+                {letter}&nbsp;
+            </span >
+           
+        );
+    });
+
+    return Scores;
+
+  }
+
   return (
-    <div className="container-fluid">
-      <Header>SCORES</Header>
-    </div>
+    <>
+      <div className ="background">
+
+      </div>
+      <div className="container-fluid">
+        <Header>{addText('S C O R E S')}</Header>
+        <ScoreBoard/>
+
+      </div>
+    </>
   );
 }
 
---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ScoreBoard = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
